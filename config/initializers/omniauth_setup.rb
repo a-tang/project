62bdd8c1c0f70a64dec0e4ee513cb582c511a648
Alex Tang <alextang3@outlook.com>
2016-06-21 18:14:09 -0700

    modify omniauth setup

--- config/initializers/omniauth_setup.rb
+++ config/initializers/omniauth_setup.rb
@@ -1,7 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.secrets.twitter_consumer_key, Rails.application.secrets.twitter_consumer_secret
+  provider :twitter,
+  ENV["twitter_consumer_key"],
+  ENV['twitter_consumer_secret']
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.facebook_application_id, Rails.application.secrets.facebook_secret_app_key
+  provider :facebook,
+  ENV['facebook_application_id'],
+  ENV['facebook_secret_app_key']
 end
